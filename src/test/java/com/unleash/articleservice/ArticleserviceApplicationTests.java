7480a0f301946f547152e8ba6b4be108660601cf
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-05 10:51:02 +0530

    updated git action file

--- src/test/java/com/unleash/articleservice/ArticleserviceApplicationTests.java
+++ src/test/java/com/unleash/articleservice/ArticleserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.unleash.articleservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ArticleserviceApplicationTests {
 
 	@Test
